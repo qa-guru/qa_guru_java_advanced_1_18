ea1ab297abf25a2e7dff6b4aa3abbe024d60839a
Artem Eroshenko <eroshenkoam@me.com>
2023-04-05 13:02:10 +0300

    add tests

---
+++ app/src/androidTest/java/qa/guru/kaspresso/IncrementButtonTest.kt
@@ -0,0 +1,32 @@
+package qa.guru.kaspresso
+
+import androidx.test.ext.junit.rules.activityScenarioRule
+import com.kaspersky.components.alluresupport.withAllureSupport
+import com.kaspersky.kaspresso.kaspresso.Kaspresso
+import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import org.junit.Rule
+import org.junit.Test
+
+class IncrementButtonTest : TestCase(
+    kaspressoBuilder = Kaspresso.Builder.withAllureSupport()
+) {
+
+    @get:Rule
+    val activityRule = activityScenarioRule<MainActivity>()
+
+    @Test
+    fun incrementButtonShouldIncreaseValue() = run {
+        step("Launch the app") {
+            MainScreen {
+                incrementButton.isDisplayed()
+                incrementButton {
+                    repeat(5) {
+                        click()
+                    }
+                }
+                valueText.hasText("Count: 5")
+            }
+        }
+    }
+
+}
