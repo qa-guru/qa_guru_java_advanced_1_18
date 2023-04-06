7756309522ed312969608ef3a16caae8d2b505f3
Artem Eroshenko <eroshenkoam@me.com>
2023-04-06 10:42:33 +0300

    fix github build (via #1)

--- app/src/androidTest/java/qa/guru/kaspresso/IncrementButtonTest.kt
+++ app/src/androidTest/java/qa/guru/kaspresso/IncrementButtonTest.kt
@@ -1,6 +1,8 @@
 package qa.guru.kaspresso
 
+import android.Manifest
 import androidx.test.ext.junit.rules.activityScenarioRule
+import androidx.test.rule.GrantPermissionRule
 import com.kaspersky.components.alluresupport.withAllureSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
@@ -14,6 +16,11 @@ class IncrementButtonTest : TestCase(
     @get:Rule
     val activityRule = activityScenarioRule<MainActivity>()
 
+    @get:Rule
+    val runtimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    )
     @Test
     fun incrementButtonShouldIncreaseValue() = run {
         step("Launch the app") {
