6b6ac69c77081d5f8722a83d89ef4ae11ddce360
Artem Eroshenko <eroshenkoam@me.com>
2023-04-10 23:24:15 +0300

    fix gradle command

--- debug.sh
+++ debug.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 adb -s emulator-5554 exec-out sh -c "ls -al /sdcard/Documents"
+adb -s emulator-5554 exec-out sh -c "ls -al /sdcard/Documents/video"
 adb -s emulator-5554 exec-out sh -c "ls -al /sdcard/googletest/test_outputfiles"
