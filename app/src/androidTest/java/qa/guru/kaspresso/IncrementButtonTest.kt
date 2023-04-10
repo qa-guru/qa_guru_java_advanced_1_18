285ad9693a9deff0ca3a3c6a40edc98e31f3c362
Artem Eroshenko <eroshenkoam@me.com>
2023-04-10 21:23:46 +0300

    fix allure

--- app/src/androidTest/java/qa/guru/kaspresso/IncrementButtonTest.kt
+++ app/src/androidTest/java/qa/guru/kaspresso/IncrementButtonTest.kt
@@ -1,16 +1,19 @@
 package qa.guru.kaspresso
 
 import android.Manifest
+import androidx.test.core.app.ActivityScenario
 import androidx.test.ext.junit.rules.activityScenarioRule
 import androidx.test.rule.GrantPermissionRule
 import com.kaspersky.components.alluresupport.withAllureSupport
+import com.kaspersky.components.alluresupport.withForcedAllureSupport
 import com.kaspersky.kaspresso.kaspresso.Kaspresso
 import com.kaspersky.kaspresso.testcases.api.testcase.TestCase
+import com.kaspersky.kaspresso.testcases.core.testcontext.TestContext
 import org.junit.Rule
 import org.junit.Test
 
 class IncrementButtonTest : TestCase(
-    kaspressoBuilder = Kaspresso.Builder.withAllureSupport()
+    kaspressoBuilder = Kaspresso.Builder.withForcedAllureSupport()
 ) {
 
     @get:Rule
@@ -19,7 +22,7 @@ class IncrementButtonTest : TestCase(
     @get:Rule
     val runtimePermissionRule: GrantPermissionRule = GrantPermissionRule.grant(
         Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.READ_EXTERNAL_STORAGE
+        Manifest.permission.READ_EXTERNAL_STORAGE,
     )
     @Test
     fun incrementButtonShouldIncreaseValue() = run {
